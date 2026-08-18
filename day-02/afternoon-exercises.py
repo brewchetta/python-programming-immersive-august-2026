84f841c359ac303abde19565a07ea8cae3d7bce3
brewchetta <chett.tiller@gmail.com>
2026-08-18 14:11:46 -0400

    add afternoon exercises

---
+++ day-02/afternoon-exercises.py
@@ -0,0 +1,54 @@
+# python -i day-02/afternoon-exercises.py
+
+"""
+1. Build a function `is_it_cake()` which accepts an argument `string` and returns `True` if the `string` is "cake" and `False` otherwise.
+"""
+
+def is_it_cake( string:str ):
+    pass
+
+
+
+"""
+2. Write a function get_average() which accepts list_of_numbers
+	Return all of those numbers averaged together. Begin by using a `for` loop or a `while` loop to add all the numbers together.
+"""
+
+def get_average( list_of_numbers:list ):
+    pass
+
+
+
+"""
+3. Write a function open_thieves_cave() which takes in one argument passphrase
+ 	the function returns True if the passphrase is "open sesame" and False if the passphrase is anything else
+ 	Example:	open_thieves_cave("open sesame") >>> True
+ 	Example:	open_thieves_cave("speak friend and enter") >>> False
+"""
+
+def open_thieves_cave( passphrase:str ):
+    pass
+
+
+
+"""
+4. Define a new function fungus_among_us() which accepts a list `plants` as an argument
+    return True if the function contains the string "fungus" and False if not
+    don't overthink this one, there's a very simple method to doing it...
+    Example: fungus_among_us( ["tree", "flower", "fungus", "moss"] ) >>> True
+"""
+
+def fungus_among_us( plants:list ):
+    pass
+
+
+
+"""
+5. Define a new function halfway_there() which accepts a list of arbitrary data `items` longer than 1 item
+    insert the string "HALFWAY" at the middle of the list and return the altered list
+    when inserting, you will need to make sure your index is an integer...
+    Example: halfway_there( [1,2,3,4,5,6] ) >>> [1,2,3,"HALFWAY",4,5,6]
+"""
+
+def halfway_there( items:list ):
+    pass
