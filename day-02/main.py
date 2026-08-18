d7ea345be6d0364ebe4f8c227e39b01c5cbb03c9
Chett Tiller <chett.tiller@gmail.com>
2026-08-18 12:38:15 -0400

    through loops and truthiness

--- day-02/main.py
+++ day-02/main.py
@@ -83,10 +83,126 @@ def check_credentials(username, password):
 
 # The Input Function #
 
+# favorite_drink = input("What is your favorite drink? ")
+
+# favorite_animal = input("What is your favorite animal? ")
+
+def check_credentials():
+    username = input("Username: ")
+    password = input("Password: ")
+    
+    if (username == valid_username and password == valid_password):
+        return "Access Granted"
+    else:
+        return "Access DENIED"
+
 
 # While Loops #
+
+animals = ["aardvark", "alligator", "bat"]
+#           0           1            2
+
+# looping - go through each item one at a time and doing something with that item
+
+def print_all_animals():
+    index = 0
+
+    while (index < 3):
+        current_animal = animals[index]
+        print( current_animal )
+        index += 1
+
+    print("all done")
+
+
+def ask_for_valid_password():
+    pw = ""
+    # this will keep asking until the pw == valid_password
+    while (pw != valid_password):
+        pw = input("What is the password? ")
+
+    print("Yes that is the password")
+
+
+def infinite_loops():
+    # this will never stop because True will never be False!
+    while(True):
+        print("This is the song that never ends")
+        print("It goes on and on my friends")
+
 
 
 # For Loops #
 
+def print_each_animal():
+    for animal in animals:
+        print(animal)
 
+def find_animal(animal_name):
+    for animal in animals:
+        if (animal_name == animal):
+            return animal
+    
+    return f"Did not find {animal_name} in animals"
+
+def filter_out_long_animal_names(threshold):
+    filtered_animals = []
+
+    for animal in animals:
+        if (len(animal) <= threshold):
+            filtered_animals.append( animal )
+    
+    return filtered_animals
+
+def has_three_letters(string):
+    return len(string) == 3
+
+def filter_animals( callback ):
+    filtered_animals = []
+
+    for animal in animals:
+        if ( callback(animal) == True ):
+            filtered_animals.append(animal)
+    
+    return filtered_animals
+
+
+# Truthiness
+# every variable that exists has an inherent truthiness to it
+
+def truthy_or_falsey(item):
+    if (item):
+        print(f"{item} is truthy!")
+    else:
+        print(f"{item} is falsey...")
+
+# string:
+"bat"   # truthy
+"   "   # truthy
+""      # falsey
+
+# numbers:
+0       # falsey
+1       # truthy
+1000    # truthy
+0.0001  # truthy
+-1      # truthy
+
+# booleans:
+True    # True
+False   # False
+
+# dictionaries:
+{ "name": "Chett" } # truthy
+{}                  # falsey
+
+# lists:
+["hello"]           # truthy
+[]                  # falsey
+
+def get_user_input():
+    name = input("What is your name? ")
+    if (name):
+        return "Continue"
+    else:
+        return "Invalid name"
