681e944dc12ca2353da27528c7e6ae27e2a5d1f9
Chett Tiller <chett.tiller@gmail.com>
2026-08-18 10:38:28 -0400

    review questions

--- day-02/review.py
+++ day-02/review.py
@@ -5,8 +5,14 @@
 # 	the function returns the first_name and last_name as a single string
 # 	Example:	create_full_name("Bob", "Marley") >>> "Bob Marley"
 
-def create_full_name():
-    pass
+def create_full_name(first_name, last_name):
+    # formatted (f) string
+    return f"{first_name} {last_name}"
+    # concatenation
+    return first_name + " " + last_name
+
+
+print( create_full_name("Chett", "Tiller") )
 
 
 # Define a new function c_to_f() which takes in one argument: temp_celsius as a number
@@ -14,5 +20,11 @@ def create_full_name():
 # 	the formula for conversion is F = (C * 9/5) + 32
 # 	Example:	c_to_f(30) >>> 86.0
 
-def c_to_f():
-	pass
+def c_to_f(temp_celsius):
+	return (temp_celsius * 9/5) + 32
+
+
+print( c_to_f(32) )
+print( c_to_f(100) )
+print( c_to_f(0) )
+print( c_to_f(50) )
