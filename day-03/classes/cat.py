19216de2160ffc3a90985b8368997f138d3e4e81
brewchetta <chett.tiller@gmail.com>
2026-08-19 14:36:44 -0400

    add cat class methods

--- day-03/classes/cat.py
+++ day-03/classes/cat.py
@@ -13,6 +13,24 @@ class Cat:
     def __repr__(self):
         return f"Cat(name={self.name}, number_of_lives={self.number_of_lives})"
 
+    def meow(self):
+        return "MEOW"
+
+    # a decorator is a special modifier that changes a function in some way
+    # decorators have the @ in front of them
+    @classmethod
+    def print_all_cat_names(cls):
+        for cat in cls.all_cats:
+            print(cat.name)
+    # cls stands for the class itself
+    # cls == Cat
+
+    @classmethod
+    def create_super_cat(cls, name):
+        new_cat = cls(name)
+        new_cat.number_of_lives = 81
+        return new_cat
+
 
 # all cats start with 9 lives but if they change their number_of_lives they track those seperately
 octavia = Cat(name="Octavia")
